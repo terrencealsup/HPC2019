988b1761c057d39e76119440786cfd5b85672ffa
Terrence Alsup <terrenceaalsup@gmail.com>
2019-04-29 15:30:20 -0400

    Added int ring

--- hw5/int_ring.cpp
+++ hw5/int_ring.cpp
@@ -6,8 +6,10 @@
 double time_pingpong(int rank, int size, long Nrepeat, long Nsize, MPI_Comm comm) {
 
   int* msg = (int*) malloc(Nsize * sizeof(int));
-  for (long i = 0; i < Nsize; i++) {
-    msg[i] = 0; // Initialize the array to 0.
+  if (rank == 0) {
+    for (long i = 0; i < Nsize; i++) {
+      msg[i] = 0; // Initialize the array to 0.
+    }
   }
 
   MPI_Barrier(comm);
@@ -19,19 +21,19 @@ double time_pingpong(int rank, int size, long Nrepeat, long Nsize, MPI_Comm comm
     MPI_Status status;
 
     // Send the initial message from process 0.
-    if (i == 0 && rank == 0) {
-      MPI_Send(msg, Nsize, MPI_INT, 1, repeat, comm)
+    if (repeat == 0 && rank == 0) {
+      MPI_Send(msg, Nsize, MPI_INT, 1, repeat, comm);
     } else{
       int src  = (rank - 1) % size;  // Who is delivering the message?
       int dest = (rank + 1) % size;  // Where are we sending the message to?
-      MPI_Recv(msg, Nsize, MPI_INT, src, repeat, comm);
+      MPI_Recv(msg, Nsize, MPI_INT, src, repeat, comm, &status);
 
       // Update each element by its rank.
       for (long j = 0; j < Nsize; j++) {
         msg[j] += rank;
       }
       // Send the message updated by the rank.
-      MPI_Send(msg, Nsize, MPI_INT, dest, repeat, comm, &status);
+      MPI_Send(msg, Nsize, MPI_INT, dest, repeat, comm);
 
     }
   }
@@ -39,9 +41,9 @@ double time_pingpong(int rank, int size, long Nrepeat, long Nsize, MPI_Comm comm
   tt = MPI_Wtime() - tt;
 
   // Now compute the error from the expected value.
-  int true_val = Nrepeats * Nsize * (Nsize - 1) / 2;
+  int true_val = Nrepeat * Nsize * (Nsize - 1) / 2;
   if (rank == 0) {
-    printf("Error = %d", abs(true_val - msg[0]))
+    printf("Error = %d", abs(true_val - msg[0]));
   }
 
   free(msg);
@@ -57,13 +59,13 @@ int main(int argc, char** argv) {
   MPI_Comm_size(comm, &size);
 
   // Latency, transferring only 1 integer.
-  long Nrepeat = 1000;
+  long Nrepeat = 10;
   double tt = time_pingpong(rank, size, Nrepeat, 1, comm);
   if (!rank) printf("int_ring latency: %e ms\n", tt/Nrepeat * 1000);
 
   // Bandwidth for a large array of Nsize integers.
   Nrepeat = 1000;
-  long Nsize = 1000000;
+  long Nsize = 10;
   tt = time_pingpong(rank, size, Nrepeat, Nsize, comm);
   if (!rank) printf("int_ring bandwidth: %e GB/s\n", (Nsize*Nrepeat)/tt/1e9);
 
