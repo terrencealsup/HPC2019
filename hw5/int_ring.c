1bd8ef809f8048ad87c2b5cb72f18346a07bcf4a
Terrence Alsup <alsup@box774.cims.nyu.edu>
2019-04-29 12:51:21 -0400

    added int_ring.c

--- hw5/int_ring.c
+++ hw5/int_ring.c
@@ -1,42 +1,71 @@
 #include <stdio.h>
+#include <cstdlib>
 #include <mpi.h>
+#include <iostream>
 
-int main(int argc, char *argv[]) {
-  int rank; // Which process is running this.
-  int size; // Get the number of processes.
-  int N = 100; // How many times to loop over all processors.
+double time_pingpong(int rank, int size, long Nrepeat, long Nsize, MPI_Comm comm) {
 
-
-  MPI_Init(&argc, &argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-  // Loop over all of the processors.
-  for (int i = 0; i < size; i++) {
-
-
+  int* msg = (int*) malloc(Nsize * sizeof(int));
+  for (long i = 0; i < Nsize; i++) {
+    msg[i] = 0; // Initialize the array to 0.
   }
-  if (rank == 0) {
-    int message_out = 42;
-    int message_in;
+
+  MPI_Barrier(comm);
+  double tt = MPI_Wtime();
+
+
+
+  for (long repeat  = 0; repeat < Nrepeat; repeat++) {
     MPI_Status status;
 
-    MPI_Recv(&message_in,  1, MPI_INT, 2, 999, MPI_COMM_WORLD, &status);
-    MPI_Send(&message_out, 1, MPI_INT, 2, 999, MPI_COMM_WORLD);
+    // Send the initial message from process 0.
+    if (i == 0 && rank == 0) {
+      MPI_Send(msg, Nsize, MPI_INT, 1, repeat, comm)
+    } else{
+      int src  = (rank - 1) % size;  // Who is delivering the message?
+      int dest = (rank + 1) % size;  // Where are we sending the message to?
+      MPI_Recv(msg, Nsize, MPI_INT, src, repeat, comm);
 
-    printf("Rank %d received %d\n", rank, message_in);
-  } else if (rank == 2) {
-    int message_out = 80;
-    int message_in;
-    MPI_Status status_2;
+      // Update each element by its rank.
+      for (long j = 0; j < Nsize; j++) {
+        msg[j] += rank;
+      }
+      // Send the message updated by the rank.
+      MPI_Send(msg, Nsize, MPI_INT, dest, repeat, comm, &status);
 
-    MPI_Send(&message_out, 1, MPI_INT, 0, 999, MPI_COMM_WORLD);
-    MPI_Recv(&message_in,  1, MPI_INT, 0, 999, MPI_COMM_WORLD, &status_2);
+    }
+  }
+  MPI_Barrier(comm);
+  tt = MPI_Wtime() - tt;
 
-    printf("Rank %d received %d\n", rank, message_in);
+  // Now compute the error from the expected value.
+  int true_val = Nrepeats * Nsize * (Nsize - 1) / 2;
+  if (rank == 0) {
+    printf("Error = %d", abs(true_val - msg[0]))
   }
 
-  MPI_Finalize();
+  free(msg);
+  return tt;
+}
 
-  return 0;
+int main(int argc, char** argv) {
+  MPI_Init(&argc, &argv);
+
+  int rank, size;
+  MPI_Comm comm = MPI_COMM_WORLD;
+  MPI_Comm_rank(comm, &rank);
+  MPI_Comm_size(comm, &size);
+
+  // Latency, transferring only 1 integer.
+  long Nrepeat = 1000;
+  double tt = time_pingpong(rank, size, Nrepeat, 1, comm);
+  if (!rank) printf("int_ring latency: %e ms\n", tt/Nrepeat * 1000);
+
+  // Bandwidth for a large array of Nsize integers.
+  Nrepeat = 1000;
+  long Nsize = 1000000;
+  tt = time_pingpong(rank, size, Nrepeat, Nsize, comm);
+  if (!rank) printf("int_ring bandwidth: %e GB/s\n", (Nsize*Nrepeat)/tt/1e9);
+
+  MPI_Finalize();
 }
