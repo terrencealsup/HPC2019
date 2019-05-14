473c0cdf313d2c9be9b4d732f09d578605a0e9c2
Terrence Alsup <alsup@box774.cims.nyu.edu>
2019-05-14 11:26:50 -0400

    update parameter of sample sort

--- hw6/ssort.cpp
+++ hw6/ssort.cpp
@@ -1,4 +1,6 @@
 // Parallel sample sort
+//
+// Author: Terrence Alsup
 #include <stdio.h>
 #include <unistd.h>
 #include <mpi.h>
@@ -14,7 +16,7 @@ int main( int argc, char *argv[]) {
 
   // Number of random numbers per processor (this should be increased
   // for actual tests or could be passed in through the command line
-  int N = 10;
+  int N = 100000;
 
   int* vec = (int*)malloc(N*sizeof(int));
   // seed random number generator differently on every core
